047951ca16f822847850dc7ddce504c050536f55
IreneAragon <irenearagongomez@gmail.com>
2021-04-14 12:37:56 +0200

    estilos chat

--- areaPaciente/detalleConversacion.php
+++ areaPaciente/detalleConversacion.php
@@ -40,6 +40,7 @@ require_once '../include/navPacientes.php';
     <div class="emisor">
         <div class="msg-emisor">
             Hola doctor, me duele la cabeza, qué puedo tomar?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             <small class="ml-3">19:43</small>
         </div>
 
@@ -49,6 +50,7 @@ require_once '../include/navPacientes.php';
     <div class="receptor">
         <div class="msg-receptor">
             Puede tomar paracetamol
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.s
             <small class="ml-3">19:45</small>
         </div>
     </div>
