48de0f27177d20b275da38bfa6fe2a791cd0c16b
IreneAragon <irenearagongomez@gmail.com>
2021-04-16 13:32:28 +0200

    pico bocadillo receptor OK.

--- areaPaciente/mensajeria/js/listarMensajes.js
+++ areaPaciente/mensajeria/js/listarMensajes.js
@@ -89,32 +89,23 @@ function listarMensajes(id_chat) {
                 classContenedor = 'receptor contenedor-receptor';
             }
 
-
-
-
-
             let pico = '';
 
             if (classContenedor === "emisor contenedor-emisor"){
-                // pico = '';
                 pico = '<span data-testid="tail-out" data-icon="tail-out" class="pico">'+
-                                '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8 13" width="8" height="13">'+
-                                    '<path opacity="0" d="M5.188 1H0v11.193l6.467-8.625C7.526 2.156 6.958 1 5.188 1z"></path>' +
-                                    '<path fill="rgba(128, 216, 255, 0.7)" d="M5.188 0H0v11.193l6.467-8.625C7.526 1.156 6.958 0 5.188 0z"></path>'+
-                                '</svg>' +
-                            '</span>';
+                            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8 13" width="8" height="13">'+
+                                '<path opacity="0" d="M5.188 1H0v11.193l6.467-8.625C7.526 2.156 6.958 1 5.188 1z"></path>' +
+                                '<path fill="rgba(128, 216, 255, 0.7)" d="M5.188 0H0v11.193l6.467-8.625C7.526 1.156 6.958 0 5.188 0z"></path>'+
+                            '</svg>' +
+                        '</span>';
             } else if (classContenedor === "receptor contenedor-receptor") {
-                pico = '';
-                // pico = '<span data-testid="tail-out" data-icon="tail-out" class="_1bUzr">'+
-                //                 '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8 13" width="8" height="13">'+
-                //                     '<path opacity="0" d="M5.188 1H0v11.193l6.467-8.625C7.526 2.156 6.958 1 5.188 1z"></path>' +
-                //                     '<path fill="rgba(128, 216, 255, 0.7)" d="M5.188 0H0v11.193l6.467-8.625C7.526 1.156 6.958 0 5.188 0z"></path>'+
-                //                 '</svg>' +
-                //             '</span>';
+                pico = '<span data-testid="tail-out" data-icon="tail-out" class="pico-receptor">'+
+                            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8 13" width="8" height="13">'+
+                                '<path opacity="0" d="M1.533 3.568L8 12.193V1H2.812C1.042 1 .474 2.156 1.533 3.568z"></path>' +
+                                '<path fill="rgba(222, 243, 250, 0.7)" d="M1.533 2.568L8 11.193V0H2.812C1.042 0 .474 1.156 1.533 2.568z"></path>'+
+                            '</svg>' +
+                        '</span>';
             }
-
-
-
 
             html += '<div class="'+classContenedor+'">'+
                         '<div class="msg contenedor-pico">' +
