8a438d80f0beceaa13ace0855dc9f095f76b0154
IreneAragon <irenearagongomez@gmail.com>
2021-04-30 11:10:50 +0200

    solved fix - areaPaciente, no eliminaba una conversación, TODO OK

--- areaPaciente/mensajeria/js/listarConversacionesPaciente.js
+++ areaPaciente/mensajeria/js/listarConversacionesPaciente.js
@@ -36,7 +36,7 @@ function listarConversaciones(){
                         "<td>"+ conversacion.nombre + " " +conversacion.apellidos + "</td>"+
                         "<td>"+ conversacion.asunto +"</td>"+
                         "<td> <a href='detalleConversacion.php?chat="+conversacion.id_correo+"' data-idConversacion='"+conversacion.id_correo+"' class='btn btn-info btn-sm btnStyle'>Ver <i class='fa fa-eye iconoOjo'></i></a> </td>"+
-                        "<td> <button type='button' data-idConversacion='"+conversacion.id_correo+"' onclick='modalEliminarConver("+conversacion.id_correo+")' class='btn btn-danger btn-sm btnStyle' data-toggle='modal' data-target='#eliminarConversacionProf'>Eliminar <i class='fa fa-trash iconoOjo'></i></button> </td>"+
+                        "<td> <button type='button' data-idConversacion='"+conversacion.id_correo+"' onclick='modalEliminarConver("+conversacion.id_correo+")' class='btn btn-danger btn-sm btnStyle' data-toggle='modal' data-target='#eliminarConversacion'>Eliminar <i class='fa fa-trash iconoOjo'></i></button> </td>"+
                       "</tr>";
         });
         $('#listaConversacionesPaciente').html(htmlTr);
@@ -46,6 +46,7 @@ function listarConversaciones(){
 /* ELIMINAR CONVERSACIÓN */
 /* Asignar al botón de borrado el ID de la conversación a eliminar */
 function modalEliminarConver(idConver) {
+    console.log('entra modal');
     btnEliminarConver.setAttribute('data-idConver',idConver);
 }
 
