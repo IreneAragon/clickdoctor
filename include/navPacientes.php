e92f744021703c6c688e30fdb34c6761590d66d1
IreneAragon <irenearagongomez@gmail.com>
2021-05-01 11:29:00 +0200

    clase hover al nav pacientes

--- include/navPacientes.php
+++ include/navPacientes.php
@@ -16,9 +16,6 @@ $srcImgPerfil = '../areaPaciente/perfil/'.$datosPaciente['srcImg'];
 <div id="navegador">
     <nav class="navbar light-blue accent-1 mynav fixed-top scrolling-navbar">
         <i class="fas fa-bars openbtn light-blue accent-1" onclick="openNav()"></i>
-        <!-- <i id="openNav" class="fas fa-bars openbtn light-blue accent-1" ></i> -->
-        <!-- <button class="openbtn light-blue accent-1" onclick="openNav()">☰</button> -->
-        <!-- <button class="openbtn light-blue accent-1" id="openNav">☰</button> -->
         <a class="navbar-brand" href="areaPaciente.php">
             <img class="img-fluid img-logo" src="../img/logoNav.png" class="float-right" height="20" alt="click doctor logo" title="Ir a inicio">
         </a>
@@ -28,12 +25,11 @@ $srcImgPerfil = '../areaPaciente/perfil/'.$datosPaciente['srcImg'];
         <div id="mySidebar" class="sidebar light-blue accent-1 ">
 
             <a class="closebtn" onclick="closeNav()">×</a>
-            <!-- <a id="closeNav" class="closebtn">×</a> -->
-            <a href="citasPaciente.php">Citas</a>
-            <a href="mensajesPaciente.php">Mensajes</a>
-            <a href="historialPaciente.php">Historial</a>
-            <a href="perfil.php">Perfil</a>
-            <a href="../cerrarSesion.php">Salir</a>
+            <a class="hover" href="citasPaciente.php">Citas</a>
+            <a class="hover" href="mensajesPaciente.php">Mensajes</a>
+            <a class="hover" href="historialPaciente.php">Historial</a>
+            <a class="hover" href="perfil.php">Perfil</a>
+            <a class="hover" href="../cerrarSesion.php">Salir</a>
         </div>
     </nav>
 </div>
