912939b787f6504b7644e5eaef8c63dd76607a86
IreneAragon <irenearagongomez@gmail.com>
2021-05-01 10:46:34 +0200

    comentario borrado

--- areaPaciente/back/editarCitaPaciente.php
+++ areaPaciente/back/editarCitaPaciente.php
@@ -12,7 +12,5 @@ $fecha_cita      = filter_input(INPUT_POST, "fecha", FILTER_DEFAULT);
 $hora_cita       = filter_input(INPUT_POST, "hora", FILTER_DEFAULT);
 $fechaDB         = date("Y-m-d", strtotime($fecha_cita) );
 
-// $consulta = 'UPDATE citas SET fecha = '.$fechaDB.', orden ='.$hora_cita .' WHERE id_cita ='.$id_cita ;
-
 $editarCitaPaciente = DB::editarCitaPaciente($id_cita, $fechaDB, $hora_cita);
 echo json_encode(array('success' => $editarCitaPaciente));
